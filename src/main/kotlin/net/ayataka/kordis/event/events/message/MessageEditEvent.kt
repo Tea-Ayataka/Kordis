cd8d103f711f61769b4e5472100ada0599591a82
Tea-Ayataka <me@ayataka.net>
2018-11-16 14:03:17 +0900

    Add MessageEditEvent::server

--- src/main/kotlin/net/ayataka/kordis/event/events/message/MessageEditEvent.kt
+++ src/main/kotlin/net/ayataka/kordis/event/events/message/MessageEditEvent.kt
@@ -2,6 +2,11 @@ package net.ayataka.kordis.event.events.message
 
 import net.ayataka.kordis.entity.channel.TextChannel
 import net.ayataka.kordis.entity.message.Message
+import net.ayataka.kordis.entity.server.Server
+import net.ayataka.kordis.entity.server.channel.text.ServerTextChannel
 import net.ayataka.kordis.event.Event
 
-class MessageEditEvent(val channel: TextChannel, val messageId: Long, val message: Message?) : Event
+class MessageEditEvent(val channel: TextChannel, val messageId: Long, val message: Message?) : Event {
+    val server: Server?
+        get() = (channel as? ServerTextChannel)?.server
+}
