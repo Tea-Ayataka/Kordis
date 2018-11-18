0ee61336b8023ef4d9afc79404dc186c6d7399f5
Tea-Ayataka <me@ayataka.net>
2018-11-18 19:13:31 +0900

    Fix user avatar handling

--- src/main/kotlin/net/ayataka/kordis/entity/user/UserImpl.kt
+++ src/main/kotlin/net/ayataka/kordis/entity/user/UserImpl.kt
@@ -6,6 +6,7 @@ import net.ayataka.kordis.entity.DiscordEntity
 import net.ayataka.kordis.entity.Updatable
 import net.ayataka.kordis.entity.image.Image
 import net.ayataka.kordis.entity.image.ImageImpl
+import net.ayataka.kordis.utils.asStringOrNull
 import net.ayataka.kordis.utils.getOrNull
 
 class UserImpl(client: DiscordClientImpl, json: JsonObject) : User, Updatable, DiscordEntity(client, json["id"].asLong) {
@@ -21,7 +22,7 @@ class UserImpl(client: DiscordClientImpl, json: JsonObject) : User, Updatable, D
     override fun update(json: JsonObject) {
         name = json["username"].asString
         discriminator = json["discriminator"].asString
-        avatar = ImageImpl.avatar(id, json["avatar"].asString)
+        json["avatar"].asStringOrNull?.let { avatar = ImageImpl.avatar(id, it) }
         json.getOrNull("bot")?.let { bot = it.asBoolean }
     }
 
