d71a3d7e55fbaa213898f793b71cb7485568964c
Tea-Ayataka <me@ayataka.net>
2021-06-15 19:37:03 +0900

    Remove incorrect test class

--- src/test/kotlin/net/ayataka/kordis/test/Test.kt
+++ src/test/kotlin/net/ayataka/kordis/test/Test.kt
@@ -1,21 +1,7 @@
 package net.ayataka.kordis.test
 
-import com.google.gson.Gson
-import com.google.gson.JsonObject
 import kotlinx.coroutines.runBlocking
-import net.ayataka.kordis.ConnectionStatus
 import net.ayataka.kordis.DiscordClient
-import net.ayataka.kordis.DiscordClientImpl
-import net.ayataka.kordis.entity.channel.PrivateTextChannel
-import net.ayataka.kordis.entity.collection.EntitySet
-import net.ayataka.kordis.entity.collection.NameableEntitySet
-import net.ayataka.kordis.entity.server.Server
-import net.ayataka.kordis.entity.server.ServerImpl
-import net.ayataka.kordis.entity.server.enums.ActivityType
-import net.ayataka.kordis.entity.server.enums.UserStatus
-import net.ayataka.kordis.entity.user.User
-import java.io.File
-import java.nio.file.Paths
 import kotlin.test.Test
 
 @Suppress("ControlFlowWithEmptyBody")
@@ -25,15 +11,6 @@ class Test {
     @Test
     fun start() = runBlocking {
 
-        val fakeClient = DiscordClientImpl("", 0, 0, emptySet())
 
-        val server = ServerImpl(fakeClient, -1)
-        val json = File("dump.json").readText(Charsets.UTF_8)
-        server.update(Gson().fromJson(json, JsonObject::class.java))
-
-        println("${server.channels.size} channels")
-        server.channels.forEach {
-            println(it.toString())
-        }
     }
 }
